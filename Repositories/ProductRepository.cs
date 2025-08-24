1a574fd193e8fcfc08b5b27dfccf07ae8fa78c33
ahmad sawaed <ahmadsawaed@ahmads-MacBook-Pro.local>
2025-08-24 18:26:44 +0300

    first commit

---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using Backend.Models;
+using Backend.Services;
+
+namespace Backend.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<string> AddPoductToDatabase(Product product);
+        List<Product> GetAll();
+    }
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IMongoDBService<Product> _mongoService;
+        const string collectionName = "products";
+        public ProductRepository(IMongoDBService<Product> mongoService)
+        {
+            _mongoService = mongoService;
+        }
+        public async Task<string> AddPoductToDatabase(Product product)
+        {
+            product.Id = Guid.NewGuid().ToString("N");
+            return await _mongoService.AddToDatabase(product,collectionName);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _mongoService.GetAll(collectionName);
+        }
+    }
+}
---
+++ Services/MongoDBService.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+
+
+namespace Backend.Services
+{
+    public interface IMongoDBService<T>
+    {
+        Task<string> AddToDatabase(T t, string collectioName);
+        List<T> GetAll(string collectioName);
+    }
+    public class MongoDBService<T> : IMongoDBService<T>
+    {
+        private readonly IConfiguration _config;
+
+        public MongoDBService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IMongoDatabase DbConnection()
+        {
+            MongoClient mongoClient;
+            var connectionString = _config["MongoDb:ConnectionString"]!;
+
+            mongoClient = new MongoClient(connectionString);
+            var db = mongoClient.GetDatabase(_config["MongoDb:DatabaseName"]!);
+            return db;
+        }
+
+        public async Task<string> AddToDatabase(T t, string collectioName)
+        {
+            try
+            {
+                var collection = DbConnection().GetCollection<T>(collectioName);
+
+                await collection.InsertOneAsync(t);
+
+                var _id = t!.GetType().GetProperty("Id")!.GetValue(t)!.ToString();
+                return _id!;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public List<T> GetAll(string collectioName)
+        {
+            var collection = DbConnection().GetCollection<T>(collectioName);
+            var filter = Builders<T>.Filter.Empty;
+
+
+            var items = collection.Find(filter).ToList();
+
+            return items;
+
+        }
+    }
+}

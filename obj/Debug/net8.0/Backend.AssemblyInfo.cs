2baf7b5b36266530fb04b9ce542d4658b4df6302
ahmad sawaed <ahmadsawaed@ahmads-MacBook-Pro.local>
2025-08-28 18:45:30 +0300

    remove unneccary code

--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f45c8d57de660e0618df875ccabeff8d9cbe114")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a574fd193e8fcfc08b5b27dfccf07ae8fa78c33")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
